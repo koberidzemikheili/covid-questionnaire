bfc06a2d03699c70b34abd52ef1a4931da827518
koberidzemikheili <mikheil.koberidze@redberry.ge>
2023-05-08 17:19:02 +0400

    feat: add tailwind and remove unnecessary files

---
+++ src/views/IdentificationView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Identification Page</h1>
+    <router-link to="/">Previous</router-link>
+    <router-link to="/questions"> Next</router-link>
+  </div>
+</template>
---
+++ src/views/StartView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex justify-center items-center h-screen">
+    <router-link to="/identification">Next</router-link>
+  </div>
+</template>
