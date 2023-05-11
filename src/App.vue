6a139f0db75aafa0229f9d0b15cc3fe2c8d6c655
koberidzemikheili <mikheil.koberidze@redberry.ge>
2023-05-11 16:43:57 +0400

    feat: add transition animations

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,25 @@ import { RouterView } from "vue-router";
 </script>
 
 <template>
-  <RouterView />
+  <div>
+    <router-view v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
+  </div>
 </template>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
 
 <style>
 body {
--- src/views/IdentificationView.vue
+++ src/views/IdentificationView.vue
@@ -1,65 +1,68 @@
 <template>
-  <div>
-    <div class="mx-32 mt-10 py-10">
-      <div class="mb-10">
-        <img
-          src="../assets/images/redberry.png"
-          alt="Left image"
-          class="float-left"
-        />
-        <img
-          src="../assets/images/right1.png"
-          alt="Right image"
-          class="float-right"
-        />
-      </div>
-      <hr class="h-px mt-8 border-1 border-black" />
-      <br />
-      <div class="flex flex-wrap">
-        <form class="w-full md:w-1/3 max-w-lg flex flex-col">
-          <label for="name" class="font-semibold text-gray-800 mb-2 mt-4"
-            >სახელი*</label
-          >
-          <input
-            type="text"
-            id="name"
-            class="border border-gray-700 border-solid bg-transparent h-8"
-            v-model="newFirstName"
-          />
-          <span>{{ firstNameerrorMessage }}</span>
-          <label for="lastname" class="font-semibold text-gray-800 mb-2 mt-4"
-            >გვარი*</label
-          >
-          <input
-            type="text"
-            id="lastname"
-            class="border border-gray-700 border-solid bg-transparent h-8"
-            v-model="newLastName"
-          />
-          <span>{{ lastNameerrorMessage }}</span>
-          <label for="email" class="font-semibold text-gray-800 mb-2 mt-4"
-            >მეილი*</label
-          >
-          <input
-            type="text"
-            id="email"
-            class="border border-gray-700 border-solid bg-transparent h-8"
-            v-model="newEmail"
-          />
-          <span>{{ emailerrorMessage }}</span>
-        </form>
-
-        <div class="w-full md:w-2/3 flex justify-center">
+  <div class="overlay">
+    <div class="yellow-overlay"></div>
+    <div>
+      <div class="mx-32 mt-10 py-10">
+        <div class="mb-10">
           <img
-            src="../assets/images/scan2.png"
-            alt="main image"
-            class="relative bottom-28"
+            src="../assets/images/redberry.png"
+            alt="Left image"
+            class="float-left"
+          />
+          <img
+            src="../assets/images/right1.png"
+            alt="Right image"
+            class="float-right"
           />
         </div>
-        <div class="w-full flex justify-center">
-          <button @click="Submit">
-            <img src="../assets/images/Vector 2.png" alt="arrow right" />
-          </button>
+        <hr class="h-px mt-8 border-1 border-black" />
+        <br />
+        <div class="flex flex-wrap">
+          <form class="w-full md:w-1/3 max-w-lg flex flex-col">
+            <label for="name" class="font-semibold text-gray-800 mb-2 mt-4"
+              >სახელი*</label
+            >
+            <input
+              type="text"
+              id="name"
+              class="border border-gray-700 border-solid bg-transparent h-8"
+              v-model="newFirstName"
+            />
+            <span>{{ firstNameerrorMessage }}</span>
+            <label for="lastname" class="font-semibold text-gray-800 mb-2 mt-4"
+              >გვარი*</label
+            >
+            <input
+              type="text"
+              id="lastname"
+              class="border border-gray-700 border-solid bg-transparent h-8"
+              v-model="newLastName"
+            />
+            <span>{{ lastNameerrorMessage }}</span>
+            <label for="email" class="font-semibold text-gray-800 mb-2 mt-4"
+              >მეილი*</label
+            >
+            <input
+              type="text"
+              id="email"
+              class="border border-gray-700 border-solid bg-transparent h-8"
+              v-model="newEmail"
+            />
+            <span>{{ emailerrorMessage }}</span>
+          </form>
+
+          <div class="w-full md:w-2/3 flex justify-center">
+            <img
+              src="../assets/images/scan2.png"
+              alt="main image"
+              class="relative bottom-28"
+            />
+          </div>
+          <div class="w-full flex justify-center">
+            <button @click="Submit">
+              <img src="../assets/images/Vector 2.png" alt="arrow right" />
+            </button>
+          </div>
         </div>
       </div>
     </div>
@@ -137,3 +140,46 @@ export default {
   },
 };
 </script>
+<style>
+.overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 999;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.yellow-overlay {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 200px;
+  height: 200px;
+  background-color: yellow;
+  opacity: 0.5;
+}
+
+@keyframes move-overlay {
+  0% {
+    transform: translate(-50%, -50%);
+    width: 100px;
+    height: 100px;
+  }
+  100% {
+    transform: translate(-5%, -450%);
+    width: 400px;
+    height: 50px;
+  }
+}
+
+.yellow-overlay {
+  animation: move-overlay 0.6s ease-in-out forwards;
+  width: 400px;
+  height: 50px;
+}
+</style>
